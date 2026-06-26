d14b23dde5181a1cff116fa81d983dc7f8593f2a
anchal <anchdel06@gmail.com>
2026-06-27 03:57:01 +0530

    admin fix hojaoooo

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -10,11 +10,9 @@ if (!admin.apps.length) {
       clientEmail:
         process.env.FIREBASE_CLIENT_EMAIL,
 
-      privateKey:
-        process.env.FIREBASE_PRIVATE_KEY.replace(
-          /\\n/g,
-          "\n"
-        ),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        : undefined,
     }),
   });
 } else {
