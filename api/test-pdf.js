37c00048d868f7c37b5c67bf2c449b2a3cc79272
anchal <anchdel06@gmail.com>
2026-06-27 23:40:32 +0530

    test 2

--- api/test-pdf.js
+++ api/test-pdf.js
@@ -1,6 +1,18 @@
+import {
+  S3Client,
+} from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
 export default async function handler(req, res) {
   return res.status(200).json({
     success: true,
-    message: "API is working",
+    aws: "loaded",
   });
 }
