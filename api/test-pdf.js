fddd294f49d00c45c293b970dbd2782f52a5454f
anchal <anchdel06@gmail.com>
2026-06-27 23:42:54 +0530

    test 3

--- api/test-pdf.js
+++ api/test-pdf.js
@@ -1,18 +1,8 @@
-import {
-  S3Client,
-} from "@aws-sdk/client-s3";
-
-const s3 = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+import pdf from "pdf-parse";
 
 export default async function handler(req, res) {
   return res.status(200).json({
     success: true,
-    aws: "loaded",
+    pdf: typeof pdf,
   });
 }
